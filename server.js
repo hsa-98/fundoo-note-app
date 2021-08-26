8031ff8a1e8a3126f1902be177661aa99b736343
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-08-27 00:48:21 +0530

    feat:add cors

--- server.js
+++ server.js
@@ -6,17 +6,38 @@ const swaggerDoc = require('./swagger/swagger.json');
 const cors = require('cors');
 const app = express();
 
-app.use(cors);
+const whiteList = ["http://localhost:4000/resetpassword","http://localhost:4000/register",
+"http://localhost:4000/login"
+];
+
+/*const corsOptionsDelegate = (req,callback)=>{
+    let corsOptions;
+    console.log(req.header);
+    if(whiteList.indexOf(req.header('Origin')) !== -1){
+        corsOptions = { origin : true}
+    }
+    else{
+        corsOptions = { origin : false}
+    }
+    callback(null,corsOptions);
+}*/
+app.use(cors({
+    origin: ["http://localhost:4000/resetpassword","http://localhost:4000/register",
+    "http://localhost:4000/login"
+    ]
+
+}));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDoc));
 dbConfig.dbConnection();
 
-app.get('/',(req,res) => {
+app.get('/',/*cors(corsOptionsDelegate),*/(req,res) => {
     res.json({"message":"Welcome to fundoo notes app"});
 });
 
 require('./app/routes/note.routes')(app);
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDoc));
 
 app.listen(process.env.PORT,() =>{
     console.log("App is listening on port number",process.env.PORT);
